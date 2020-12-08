59655a34b7b261985d5588dce2ba12c8ce8b4928
Bernardo Mesko <bemesko@gmail.com>
2020-12-08 11:18:27 -0300

    Score na tela e persistência de dados

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Score : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //TODO refatorar isso aqui inteiro para separar responsabilidades
+    [SerializeField] private TextMeshProUGUI _scoreDisplay;
+    [SerializeField] private TextMeshProUGUI _highScoreDisplay;
+
+    private int _currentScore;
+    private int _highScore;
+
+    void Awake()
     {
-        
+        _currentScore = 0;
+        _highScore = PlayerPrefs.GetInt("highScore", 0);
+
+        _highScoreDisplay.SetText($"Recorde: {_highScore}");
+
+        InvokeRepeating("IncreaseScore", 0.3f, 0.3f);
     }
 
-    // Update is called once per frame
-    void Update()
+    void IncreaseScore()
     {
-        
+        _currentScore++;
+
+        if (_currentScore > _highScore)
+        {
+            _highScore = _currentScore;
+            PlayerPrefs.SetInt("highScore", _highScore);
+        }
+
+        _scoreDisplay.SetText($"Pontuação: {_currentScore}");
+        _highScoreDisplay.SetText($"Recorde: {_highScore}");
     }
 }
