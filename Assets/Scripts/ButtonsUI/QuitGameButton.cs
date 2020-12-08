a06f6c1f162b62e874de4d89f31c710248561fa4
Bernardo Mesko <bemesko@gmail.com>
2020-12-08 12:32:30 -0300

    Script QuitGameButton.cs

--- Assets/Scripts/ButtonsUI/QuitGameButton.cs
+++ Assets/Scripts/ButtonsUI/QuitGameButton.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class QuitGameButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnClick()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Application.Quit(0);
     }
 }
