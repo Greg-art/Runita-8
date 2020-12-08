575f5495f458eb0b59887771252e4e3da20e7481
Bernardo Mesko <bemesko@gmail.com>
2020-12-08 12:03:08 -0300

    Consertado bug das plataformas não sendo destruídas

--- Assets/Scripts/DestroyerScript.cs
+++ Assets/Scripts/DestroyerScript.cs
@@ -13,9 +13,23 @@ public class DestroyerScript : MonoBehaviour
         _gameOverCanvas.gameObject.SetActive(false);
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnCollisionEnter(Collision other)
     {
 
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
         if (other.gameObject.tag != "Player")
         {
             if (other.gameObject.transform.parent)
