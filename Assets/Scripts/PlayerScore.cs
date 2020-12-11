09be4558eef575b4d3db09763ab82e236e387b6c
Bernardo Mesko <bemesko@gmail.com>
2020-12-11 11:38:39 -0300

    Suprime avisos que os textos nunca foram atribuídos

--- Assets/Scripts/PlayerScore.cs
+++ Assets/Scripts/PlayerScore.cs
@@ -6,8 +6,8 @@ using TMPro;
 public class PlayerScore : MonoBehaviour, IHaveScore
 {
     //TODO refatorar isso aqui inteiro para separar responsabilidades
-    [SerializeField] private TextMeshProUGUI _scoreDisplay;
-    [SerializeField] private TextMeshProUGUI _highScoreDisplay;
+    [SerializeField] private TextMeshProUGUI _scoreDisplay = default;
+    [SerializeField] private TextMeshProUGUI _highScoreDisplay = default;
 
     private int _currentScore;
     private int _highScore;
@@ -18,13 +18,6 @@ public class PlayerScore : MonoBehaviour, IHaveScore
         _highScore = PlayerPrefs.GetInt("highScore", 0);
 
         _highScoreDisplay.SetText($"Recorde: {_highScore}");
-
-        InvokeRepeating("IncreaseScore", 0.3f, 0.3f);
-    }
-
-    public void IncreaseScore()
-    {
-        HandleScore(1);
     }
 
     public void HandleScore(int scoreAmount)
