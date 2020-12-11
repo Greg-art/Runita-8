2f70ae4996df12fec3b0a3fd9e2de5ef216c8374
Bernardo Mesko <bemesko@gmail.com>
2020-12-11 12:23:23 -0300

    Singleton pra pausar o jogo

---
+++ Assets/Scripts/GamePauser.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePauser : MonoBehaviour
+{
+    public static GamePauser Instance;
+    private bool _gameIsPaused = false;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    public void ChangeGamePauseStatus(bool isPaused)
+    {
+        _gameIsPaused = isPaused;
+        if (_gameIsPaused)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    public bool GetGamePauseStatus()
+    {
+        return _gameIsPaused;
+    }
+}
