d9b21697c7ac406e52241faf27fa36b8947bbb7e
Grégori Sabel <seriosabel57@gmail.com>
2020-12-10 21:40:01 -0300

    dragão voa
    script do dragão

---
+++ Assets/Scripts/FlyScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyScript : MonoBehaviour
+{
+    Rigidbody bala;
+    [SerializeField]
+    private float velocity = 5.0f;
+    void Start()
+    {
+        bala = GetComponent<Rigidbody>();
+        bala.velocity = new Vector3(velocity,0,0);
+    }
+}
