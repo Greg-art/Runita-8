d402d4de2a0a145a97fc39f02e0167fecd6f90f5
Bernardo Mesko <bemesko@gmail.com>
2020-12-13 19:47:49 -0300

    Script SoundManager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private AudioClip _portalClip;
+    [SerializeField] private AudioClip _deathClip;
+    [SerializeField] private AudioClip _pickupClip;
+    [SerializeField] private AudioClip _dragonClip;
+    [SerializeField] private AudioSource _audioSource;
+
+    private SoundManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        _audioSource = GetComponent<AudioSource>();
+
+    }
+}
